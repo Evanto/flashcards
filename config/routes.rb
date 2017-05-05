edf15b1f919a4f2ecdfeb3cd39c09f7e8d1332c8
Eva Medvedeva <ohmywob@gmail.com>
2017-05-05 21:03:06 +0300

    add show/create/edit/update/destroy actions, add edit/destroy links to the show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
    root "page#index"
-   resources :cards, only: [:index]
-
+   resources :cards
+   #get 'cards/:id/', to: 'cards#show'
    #, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
