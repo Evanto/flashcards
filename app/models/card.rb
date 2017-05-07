569a81177685a789759c488f3151e7eb613a6cbc
Eva Medvedeva <ohmywob@gmail.com>
2017-05-07 16:54:47 +0300

    form split into partials, /edit form makes edits

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,19 +1,13 @@
 class Card < ApplicationRecord
-  #validates :original_text, :translated_text, presence: true
-  validates  :translated_text, presence: { message: 'Ошибка ассоциации.' }
-  # validates :original_text, :translated_text, uniqueness: true
-  validate  :fields_not_equal
-#  before_validation :set_review_date_to_now, on: :create
+  validates  :translated_text, presence: { message: 'Все поля должны быть заполнены!' }
+  validate  :fields_not_equal, { message: 'Содержимое полей должно отличаться!' }
   before_create :set_review_date_to_now
 
   def fields_not_equal
-    if self.original_text.downcase == self.translated_text.downcase
-    #   self.errors.add( :original_text,:must_be_diff_w_transl)
-      # self.errors.add(:base, "Поля не должны совпадать!")
-     end
+    #if self.original_text.downcase == self.translated_text.downcase
+    errors.add(:translated_text, "can't be the same as email") if if self.original_text.downcase == self.translated_text.downcase
+     #end
   end
-
-
 
   def set_review_date_to_now
      self.review_date = Time.current
